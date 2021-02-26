f4ab85e9f644bf3b2053b4e83bf426a55424c6de
nhj1987 <nhj1987@cj.net>
2021-02-26 14:20:10 +0900

    신규커밋테스트

---
+++ src/main/java/com/example/demo/BlackhomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackhomeApplication.class, args);
+	}
+
+}
